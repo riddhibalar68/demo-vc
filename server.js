712ef5b7de89c62edf9021a88653bc0b0ddcab28
riddhi-simform <riddhi.b@simformsolutions.com>
2021-10-29 11:05:10 +0530

    init

---
+++ server.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const OpenTok = require("opentok");
+const OT = new OpenTok(process.env.API_KEY, process.env.API_SECRET);
+
+let sessions = {};
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/landing.html");
+});
+
+app.get("/session/:room", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/session/:room", (request, response) => {
+  console.log(sessions)
+  const roomName = request.params.room;
+  // Check if the session already exists
+  if (sessions[roomName]) {
+    // Generate the token
+    generateToken(roomName, response);
+  } else {
+    // If the session does not exist, create one
+    OT.createSession((error, session) => {
+      if (error) {
+        console.log("Error creating session:", error);
+      } else {
+        // Store the session in the sessions object
+        sessions[roomName] = session.sessionId;
+        // Generate the token
+        generateToken(roomName, response);
+      }
+    });
+  }
+});
+
+function generateToken(roomName, response) {
+  // Configure token options
+  const tokenOptions = {
+    role: "publisher",
+    data: `roomname=${roomName}`
+  };
+  // Generate token with the Video API Client SDK
+  let token = OT.generateToken(
+    sessions[roomName],
+    tokenOptions
+  );
+  // Send the required credentials back to to the client
+  // as a response from the fetch request
+  response.status(200);
+  response.send({
+    sessionId: sessions[roomName],
+    token: token,
+    apiKey: process.env.API_KEY
+  });
+}
+
+const listener = app.listen(3000, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
